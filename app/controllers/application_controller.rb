e2e1b18a13dea9d9022d66226272724dffbf961a
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-01 23:05:52 +0200

    finish devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
-end
+  
+  
+    def configure_devise_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u|
+      u.permit(
+               :first_name,
+               :last_name,
+               :user_name,
+               :email,
+               :password,
+               :password_confirmation
+               )
+             }
+    end
+  end
